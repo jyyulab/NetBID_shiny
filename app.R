d7d272a01b7299258842a9fa386b3ba8d1c20f84
SherryDong <xinran.dong@foxmail.com>
2021-05-27 08:36:17 +0800

    add demo in app.R

--- app.R
+++ app.R
@@ -1,3 +1,6 @@
 library(NetBIDshiny)
 NetBIDshiny.run4Vis()
 NetBIDshiny.run4MR()
+#
+NetBIDshiny.run4Vis(load_data_path = system.file('demo1','driver/DATA/analysis.par.Step.ms-tab.RData',package = "NetBID2"))
+
